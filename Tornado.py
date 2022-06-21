84ec3f8949de55a6dfa45459ab1b05f1f86413db
Xayah <zds1249475336@gmail.com>
2022-06-21 12:32:24 +0800

    feat: 添加/api/v1/app/data路由
    Change-Id: I31aa7cb181e4c1ce73d7744ae9e94eef4a4b7ef2

--- Tornado.py
+++ Tornado.py
@@ -1,8 +1,11 @@
+import json
 import os
 from abc import ABC
 
 import tornado.ioloop
 import tornado.web
+
+from util import db
 
 
 class BaseHandler(tornado.web.RequestHandler, ABC):
@@ -14,22 +17,67 @@ class BaseHandler(tornado.web.RequestHandler, ABC):
 
 class Check(BaseHandler, ABC):
     def get(self):
-        self.write({
+        self.write(json.dumps({
             'code': 1,
-            'message': "",
-        })
+            'msg': '操作成功！',
+            'data': {}
+        }, ensure_ascii=False))
+
+
+class AppData(BaseHandler, ABC):
+    def get(self):
+        data = db.get_all('data')[0]
+        self.write(json.dumps({
+            'code': 1,
+            'msg': '操作成功！',
+            'data': {
+                'state': data['state'],
+                'battery_car': data['battery_car'],
+                'battery_drone': data['battery_drone']
+            }
+        }, ensure_ascii=False))
+
+    def post(self):
+        data = db.get_all('data')[0]
+        state = data['state']
+        battery_car = data['battery_car']
+        battery_drone = data['battery_drone']
+        body_arguments = self.request.body_arguments
+        if 'state' in body_arguments:
+            state = body_arguments['state'][0].decode()
+        if 'battery_car' in body_arguments:
+            battery_car = body_arguments['battery_car'][0].decode()
+        if 'battery_drone' in body_arguments:
+            battery_drone = body_arguments['battery_drone'][0].decode()
+        db.update('data', 1, 'state', state)
+        db.update('data', 1, 'battery_car', battery_car)
+        db.update('data', 1, 'battery_drone', battery_drone)
+        self.write(json.dumps({
+            'code': 1,
+            'msg': '操作成功！',
+            'data': {
+                'state': state,
+                'battery_car': battery_car,
+                'battery_drone': battery_drone
+            }
+        }, ensure_ascii=False))
 
 
 def make_app():
     return tornado.web.Application([
         (r'/api/v1/check', Check),
+        (r'/api/v1/app/data', AppData),
     ], static_path=os.path.join(os.path.dirname(__file__), "asserts"), static_url_prefix="/asserts/")
 
 
 if __name__ == "__main__":
+    # 初始化MySQL数据库
+    db.init()
+    if len(db.get_all('data')) == 0:
+        db.insert('data', ['', '', ''])
     # 判断是否存在asserts文件夹，没有则创建
     if not os.path.exists("asserts"):
         os.mkdir("asserts")
     app = make_app()
-    app.listen(port=3399)
+    app.listen(port=33307)
     tornado.ioloop.IOLoop.current().start()
---
+++ util/db.py
@@ -0,0 +1,76 @@
+import datetime
+
+import pymysql
+
+HOST = '127.0.0.1'  # 宿主机地址
+PORT = 33306  # 宿主机地址
+USER = 'root'  # 用户名
+PASSWORD = '123456'  # 密码
+DB = 'App'  # 数据库名
+global db
+
+TABLE_DATA = 'data'  # 数据表名
+FIELD_DATA = [
+    "state text,",
+    "battery_car text,",
+    "battery_drone text",
+]  # 字段
+
+
+def timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def get_all(table):
+    """返回表中所有数据"""
+    db.ping()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("use {};".format(DB))
+    cursor.execute("SELECT * from {}".format(table))
+    data = cursor.fetchall()
+    db.commit()
+    return data
+
+
+def init():
+    """创建数据库和数据表"""
+    global db
+    db = pymysql.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, charset='utf8')
+    db.ping()
+    cursor = db.cursor()
+    # 创建数据库
+    cursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARSET utf8 COLLATE utf8_general_ci;".format(DB))
+    # 选择数据库
+    cursor.execute("use {};".format(DB))
+    # 创建数据表(DATA)
+    field_data = "".join(FIELD_DATA)
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS {}(id int primary key not null auto_increment,".format(TABLE_DATA)
+        + "created_at timestamp,updated_at timestamp,"
+        + field_data
+        + ");")
+
+
+def insert(table, data):
+    """插入一条记录"""
+    db.ping()
+    cursor = db.cursor()
+    cursor.execute("use {};".format(DB))
+    insert_data = "INSERT INTO {} VALUES (0,'{}','{}'".format(table, timestamp(), timestamp())
+    for i in data:
+        insert_data += ",'"
+        insert_data += i
+        insert_data += "'"
+    insert_data += ")"
+    cursor.execute(insert_data)
+    db.commit()
+
+
+def update(table, item_id, data_name, data):
+    """更新一条记录"""
+    db.ping()
+    cursor = db.cursor()
+    cursor.execute("use {};".format(DB))
+    cursor.execute(
+        "update {} set {} = '{}',updated_at = '{}' where id = {}".format(table, data_name, data, timestamp(), item_id))
+    db.commit()
